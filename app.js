e070a26f5792a486ff028611bae67b20e2e3fca7
Raghav Bhandari <raghavbhandarirk@gmail.com>
2023-04-21 08:18:23 +0530

    trying new

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
